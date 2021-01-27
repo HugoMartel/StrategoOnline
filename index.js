f752b053e1b6ba2863ad334462b5d731062bbafb
Hugo Martel <hugo.martel@student.yncrea.fr>
2021-01-27 22:52:50 +0100

    Added few comments to explain what's happening

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Setup requires and https keys & certificates
 let app = require('express')();
 let fs = require('fs');
 
@@ -6,10 +7,12 @@ let hsCert = fs.readFileSync('cert.pem').toString();
 
 let https = require('https').createServer({key: hsKey, cert: hsCert}, app);
 
+// Router
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/front/html/index.html');
 });
 
-https.listen(4200, function() {
-    console.log('Server is up and running!');
+// Make the server use port 4200
+let listener = https.listen(4200, function() {
+    console.log('Server is up and running on port ' + listener.address().port);
 });

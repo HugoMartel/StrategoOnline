7881a9fff306e856acc7f957fd66714e30a10274
Lionel SMETS <lionel.smets@isen.yncrea.fr>
2021-03-01 12:02:32 +0100

    added json loader/editor documentation

--- back/modules/storage.js
+++ back/modules/storage.js
@@ -1,9 +1,46 @@
+/**
+ * @file Process the load and edit of the json files
+ * @version 1.0
+ * @author Stratego Online
+ */
+
+//Requires
+const fs = require("fs");
+
+/**
+ * Process the load and edit of the json files
+ * @type {Object}
+ * @return {Object} functions to use with the AppRequest module
+ * @name Storage
+ * @namespace storage
+ */
+
 let Storage = (function () {
+
+  /**
+   * @function storage.getData
+   * @param {String} name
+   * File name request
+   * @returns {Array} 
+   * File content response
+   * @description Get the content of a JSON file
+   */
 
 let getJSONData =( function (name){
   let data = fs.readFileSync('../storage/'+name+'.json','utf8')
 return (JSON.parse(data));
 });
+
+ /**
+   * @function storage.editData
+   * @param {String} name
+   * File name request
+   * @param {Function} edit
+   * File edit function
+   * @returns {} /
+   * 
+   * @description Edit the content of a JSON file with a function
+   */
 
 let editJSONData = (function (name, edit){
   fs.readFile('../storage/'+name+'.json',function(err,content){

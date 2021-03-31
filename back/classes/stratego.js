683166111fa1432ea963439eac8c393d53bb0253
Hugo Martel <hugo.martel@student.yncrea.fr>
2021-03-31 13:39:18 +0200

    Added a variable to store who's turn it is in the Game object

--- back/classes/stratego.js
+++ back/classes/stratego.js
@@ -17,6 +17,7 @@ class Game {
     this.room_name = name;
 
     this.players = [player, null];
+    this.turn = 0;//Defaults to player 0 turn
     //Create each player's pieces array
     this.tables = [
       Array(10)
